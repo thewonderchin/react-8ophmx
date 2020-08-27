91e0d3f1bdf4626163f152dcd7142852aef5c90d
thewonderchin <corey.banaszak@gmail.com>
2020-08-27 09:17:12 -0400

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Recipe from './recipe';
 
-export default function App() {
-  return (
-    <div className="containerfluid">
-      <div id="nav" className="containerfluid">
-         <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-            <span class="navbar-toggler-icon"></span>
-           </button>
-          <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-            </ul>
-          </div>
-        </nav> 
-      </div>
-      <div id="heroPage" className="containerfluid text-center">
-        <div className="jumbotron">
-          <h1 className="display-4">Hello, world!</h1>
-          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr className="my-4"></hr>
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <p className="lead">
-            <a classNmae="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-          </p>
-        </div>
-      </div>
-      <div id="footer" className="containerfluid">
-      </div>
+const App = () => {
+
+  const APP_ID = 'd9b897d5';
+  const APP_KEY = 'a71b6348dce1eb66cf502760bcc4ff03';
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() =>{
+    getRecipes();
+  }, []);
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
+  return(
+    <div className="containerfluid text-center">
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">FIND NOW</button>
+      </form>
     </div>
   );
 }
+
+export default App;
---
+++ src/nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Clock() {
+  return <h1>hello buddy</h1>;
+}
